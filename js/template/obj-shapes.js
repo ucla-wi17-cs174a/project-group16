ac31eb500a06b1e6f4966279068d8a6c966aa534
Bryson Li <brysonli@gmail.com>
2017-03-08 18:22:09 -0800

    Finally the model is working!
    Hooray!!! :)
    
    This deals with issue #1 #5
    
    We can probably deal with #6 using the collision code
    and #7 using scale.
    
    I will clean up the code in the file and then merge it with master branch.
    
    I'll also try to find more good fish models

--- js/template/obj-shapes.js
+++ js/template/obj-shapes.js
@@ -3,15 +3,11 @@
 
 // *********** Shape From File ***********
 Declare_Any_Class( "Shape_From_File",    // First, the simplest possible Shape â€“ one triangle.  It has 3 vertices, each having their own 3D position, normal
-  { 'populate': function(filename, points_transform)        // vector, and texture-space coordinate.
+  { 'populate': function(filename, points_transform=[1,1,1])        // vector, and texture-space coordinate.
       {
           this.filename = filename;     this.points_transform = points_transform;
-          //this.meshes = {};
            // Begin downloading the mesh, and once it completes return control to our webGLStart function
-          //this.positions = [];
-          //this.ready = false;
-          //this.normals = [];
-          //this.texture_coords = [];
+
           this.webGLStart = function (meshes)
           {
             for (var j = 0; j < meshes.mesh.vertices.length / 3; j++) {
@@ -21,13 +17,15 @@ Declare_Any_Class( "Shape_From_File",    // First, the simplest possible Shape â
               this.texture_coords.push(vec2(meshes.mesh.textures[2 * j], meshes.mesh.textures[2 * j + 1]));
             }
             this.indices = meshes.mesh.indices;
-
+            
+            // Don't use this code. If you want to scale just use the model transform!
+            /*
             for (var i = 0; i < this.positions.length; i++) // Apply points_transform to all points added during this call
             {
               this.positions[i] = vec3(mult_vec(this.points_transform, vec4(this.positions[i], 1)));
               this.normals[i] = vec3(mult_vec(transpose(inverse(this.points_transform)), vec4(this.normals[i], 1)));
-            }
-
+            }*/
+            Shape.prototype.copy_onto_graphics_card.call(this);
             this.ready = true;
 
           }
@@ -42,11 +40,13 @@ Declare_Any_Class( "Shape_From_File",    // First, the simplest possible Shape â
             return fcn.bind(self)
           }(this)));*/
 
-      }/*,                                    
+      },                                    
       'draw': function( graphics_state, model_transform, material )
       { 
         if( this.ready )
+        {
           Shape.prototype.draw.call(this, graphics_state, model_transform, material);
-      }*/
+        }
+      }
 
   }, Shape )
